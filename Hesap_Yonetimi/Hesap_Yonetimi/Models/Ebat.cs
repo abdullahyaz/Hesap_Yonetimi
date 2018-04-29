b201d0e2884d6242012064abb89af60a5ee8fe84
Abdullah Kul <abdullah.yazilimpark@gmail.com>
2018-04-30 00:14:36 +0300

    WindowsForm

---
+++ Hesap_Yonetimi/Hesap_Yonetimi/Models/Ebat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hesap_Yonetimi.Models
+{
+    struct Ebat
+    {
+        public double m_Genislik { get; set; }
+        public double m_Yukesklik { get; set; }
+
+        public Ebat(double g, double y)
+        {
+            m_Genislik = g;
+            m_Yukesklik = y;
+        }
+    }
+}
---
+++ Hesap_Yonetimi/Hesap_Yonetimi/Models/ModelKagit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hesap_Yonetimi.Models
+{
+    public class ModelKagit
+    {
+        public int M_Tabaka { get; set; }
+        public string M_KagitCinsi { get; set; }
+        public double M_Sayfa { get; set; }
+        public int M_Adet { get; set; }
+        public Ebat M_Ebat { get; set; }
+        public int M_Birim { get; set; }
+        public double M_Gramaj { get; set; }
+        public double M_Top { get; set; }
+
+        public double KagitTopKontrol(int TabakaAdet)
+        {
+            return TabakaAdet;
+        }
+
+    }
+}
