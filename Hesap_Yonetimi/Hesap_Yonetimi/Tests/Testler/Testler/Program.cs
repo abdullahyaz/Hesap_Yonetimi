6e6d5c52e7384fa7beeb28be326ee89861d0970e
abdullahyaz <abdullah.yazilimpark@gmail.com>
2018-05-02 19:21:33 +0300

    Test kodlar

--- Hesap_Yonetimi/Hesap_Yonetimi/Tests/Testler/Testler/Program.cs
+++ Hesap_Yonetimi/Hesap_Yonetimi/Tests/Testler/Testler/Program.cs
@@ -13,18 +13,37 @@ namespace Testler
         static void Main(string[] args)
         {
             const double Sabit = 10000;
-            int M_Gramaj = 115;
-            int M_Tabaka = 500;
+            int[] M_Gramaj = { 90, 115, 130, 135, 170, 200, 250, 300, 350, 400 };
+            int[] M_Tabaka = { 500, 250, 100 };
+            int M_Kirim = 4;
+            int M_FormaSayfa;
 
+            // Bir top kağıdın ağırlığı
             double TopAgirligi(int g, int y)
             {
-                double TBkSonuc = g * y * M_Gramaj;
+                double TBkSonuc = g * y * M_Gramaj[1];
                 double TPSonuc = TBkSonuc / Sabit;
-                return TPSonuc * M_Tabaka;
+                return TPSonuc * M_Tabaka[0];
 
             }
 
-            Console.WriteLine(TopAgirligi(70, 100));
+            // Bir Top kağıttan çıkan kitap sayısı
+            double TopCikanKitap()
+            {
+                if (M_Kirim == 4)
+                {
+                    M_FormaSayfa = 32;
+                    return M_Tabaka[0] / M_FormaSayfa;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+
+            Console.WriteLine("Bir top kağıdın ağırlığı " + TopAgirligi(70, 100));
+            Console.WriteLine("Bir Top kağıttan çıkan kitap sayısı "+TopCikanKitap());
             Console.Read();
             
         }
